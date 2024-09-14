27ea56c0dba0e08428e95f60be0ca4908988123c
dsjohann <johannd@purdue.edu>
2024-09-14 10:44:42 -0400

    Chain System Fix
    it's much better now

--- Assets/ChainObject.cs
+++ Assets/ChainObject.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class ChainObject : MonoBehaviour
 {
     public GameObject driver;
-    private Rigidbody2D RB;
     public GameObject chainPrefab;
+    public float chainSnappiness;
+    public float chainThreshold;
+    public float adjustmentSnappiness;
+
+    private Rigidbody2D RB;
     private GameObject chainClone;
+    private bool wasChained;
+    private Vector3 offset;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+        wasChained = false;
+        offset = new Vector3 (0, 0, 0);
         //driver = null;
         RB = GetComponent<Rigidbody2D>();
         chainClone = Instantiate(chainPrefab);
@@ -20,13 +30,25 @@ public class ChainObject : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (driver && !wasChained) {
+            offset = transform.position - driver.transform.position;
+            wasChained = true;
+        }
+
         if (driver)
         {
+            // chain pos
             chainClone.SetActive(true);
             chainClone.transform.position = (transform.position + driver.transform.position) / 2f;
             chainClone.GetComponent<SpriteRenderer>().size = new Vector2((transform.position - driver.transform.position).magnitude, 0.25f);
             chainClone.transform.rotation = Quaternion.FromToRotation(transform.right, (transform.position - driver.transform.position));
-            RB.velocity = driver.GetComponent<Rigidbody2D>().velocity;
+
+            Vector3 differential = driver.transform.position + offset - transform.position;
+            RB.velocity = differential * chainSnappiness;
+            if (differential.magnitude > chainThreshold) {
+                offset -= differential * adjustmentSnappiness * Time.deltaTime;
+            }
+            
             // Turn gravity off
             RB.gravityScale = 0;
         }
@@ -34,6 +56,7 @@ public class ChainObject : MonoBehaviour
             chainClone.SetActive(false);
             // Turn gravity on
             RB.gravityScale = 1;
+            wasChained = false;
         }
     }
 }
