cd5a674ba26e9b3f8cd1b1f27e68ea0e430e9f51
dsjohann <johannd@purdue.edu>
2024-09-15 10:52:40 -0400

    ToggleBlocks and Physics
    Push block texture fix next

---
+++ Assets/SwitchBlocks/SwitchCode.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCode : MonoBehaviour
+{
+    public List<GameObject> connectedBlocks;
+    public float activeArea;
+    private LayerMask layers;
+    private bool lastState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastState = false;
+        layers = LayerMask.GetMask("Block", "Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 areaVector = new Vector3(activeArea, activeArea, 0);
+
+        bool currentState = Physics2D.OverlapArea(transform.position + areaVector, transform.position - areaVector, layers);
+        if ( currentState != lastState)
+        {
+            foreach (GameObject i in connectedBlocks) {
+                i.SetActive(!currentState);
+            }
+            lastState = currentState;
+        }
+    }
+}
