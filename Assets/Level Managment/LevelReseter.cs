1584dae4b8b697b2144575f3d787502202fa792f
dsjohann <johannd@purdue.edu>
2024-09-15 13:55:56 -0400

    Multi Switch Blocks, Chain Fix, Auto Reset
    Just all the good stuff

--- Assets/Level Managment/LevelReseter.cs
+++ Assets/Level Managment/LevelReseter.cs
@@ -6,9 +6,15 @@ using UnityEngine.SceneManagement;
 
 public class LevelReseter : MonoBehaviour
 {
+    private GameObject player;
+
+    void Start() {
+        player = GameObject.Find("Player Controller");
+    }
+    
     private void Update()
     {
-        if (Input.GetButtonDown("Reset"))
+        if (Input.GetButtonDown("Reset") || player.transform.position.y < -6)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
         }
